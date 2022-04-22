a370b839ef72e3a3332f9df05351424c63f38761
Drew Hutton Yoroshi <drew.hutton@pm.me>
2022-04-22 16:44:31 +0930

    feat: add export geoJSON modal and download

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import Footer from "./Footer";
 import Main from "./Main";
 import Header from "./Header";
+import Modals from "./Modals";
 
 const Layout = () => (
   <div className="w-screen h-screen">
     <Header />
     <Main />
     <Footer />
+    <Modals />
   </div>
 );
 
