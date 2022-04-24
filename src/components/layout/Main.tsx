bcc8508703e0b985015904b2dc7af7a0b69e0e38
Drew Hutton Yoroshi <drew.hutton@pm.me>
2022-04-25 00:55:52 +0930

    chore: add small loading indicator for slow loading urls

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,22 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import List from "../editor/List";
 import initializeMap from "../../helpers/initializeMap";
 import { useMapStore } from "../../store/main";
 import { MapOptions } from "../../store/types";
 
 const Main = () => {
+  const [loading, setLoading] = useState(true);
   const initialized = useMapStore((state) => state.initialized);
 
   // Try to initialize the map if at least url exists in map-options local storage
   useEffect(() => {
-    const mapOptions = localStorage.getItem("map-options");
-    if (mapOptions) {
-      const mapOptionsJSON = JSON.parse(mapOptions) as MapOptions;
-      if (mapOptionsJSON.url) {
-        initializeMap(mapOptionsJSON);
+    const checkInitFromLocal = async () => {
+      const mapOptions = localStorage.getItem("map-options");
+      if (mapOptions) {
+        const mapOptionsJSON = JSON.parse(mapOptions) as MapOptions;
+        if (mapOptionsJSON.url) {
+          await initializeMap(mapOptionsJSON);
+        }
       }
-    }
-  }, []);
+      setLoading(false);
+    };
+
+    checkInitFromLocal();
+  }, [setLoading]);
+
   return (
     <div className="flex flex-col md:flex-row h-auto md:h-[calc(100%-150px)]">
       <div className="w-full md:w-3/5 h-[500px] md:h-full bg-neutral-focus">
@@ -24,13 +31,18 @@ const Main = () => {
           id="map"
           className="w-full h-full flex items-center justify-center"
         >
-          {!initialized && (
+          {!initialized && !loading && (
             <h3
               onClick={() => initializeMap({})}
               className="px-4 text-3xl text-center cursor-pointer text-gray-400 hover:text-white transition-all duration-200"
             >
               Your map will appear here, <br />
               Click here to initialize a default one to play with
+            </h3>
+          )}
+          {loading && (
+            <h3 className="px-4 text-3xl text-center text-gray-400">
+              Loading your previous session, just a moment...
             </h3>
           )}
         </div>
